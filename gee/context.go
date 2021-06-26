503d2b04f70daa401d84be9a09e88aa2e811bca0
flermsie <735968781@qq.com>
2021-06-26 22:54:23 +0800

    add Trie tree -> dynamic route

--- gee/context.go
+++ gee/context.go
@@ -12,10 +12,11 @@ type Context struct {
 	// origin object
 	Writer http.ResponseWriter
 	Req    *http.Request
-	// request
+	// request info
 	Path   string
 	Method string
-	// response status
+	Params map[string]string
+	// response info
 	StatusCode int
 }
 
@@ -26,6 +27,11 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 		Path:   req.URL.Path,
 		Method: req.Method,
 	}
+}
+
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
 }
 
 func (c *Context) PostForm(key string) string {
